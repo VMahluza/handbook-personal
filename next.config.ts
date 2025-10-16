ffac24de26a6f9fd3220fbd3cc805ff489386fab
VMahluza <.env.local>
2025-10-16 09:59:11 +0200

    devops:Deployment Automation

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",          // 👈 tells Next.js to generate /out instead of /.next
+  trailingSlash: true,
   images: { unoptimized: true }  // 👈 needed if you use <Image> component
 };
 
